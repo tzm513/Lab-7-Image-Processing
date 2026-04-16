7e6953ab73afd26c2bbcced0fd79683bd25d18f5
Louis Nuttall <tzm513@york.ac.uk>
2026-04-16 11:27:26 +0100

    FFT_Edge created Performs a fast fourier transform using the fftw library, and then removes low frequency signals, and recompiles the image to perform an edge detection

---
+++ FFT_Edge/fft.f90
@@ -0,0 +1,50 @@
+program FFT_edge
+    use consts
+    use pgm_processing
+    use FFTW_setup
+
+    integer, allocatable            :: image(:,:)
+    integer                         :: Nx, Ny
+    integer                         :: cx, cy
+    real(kind=dp)                   :: filter
+    complex(kind=dp), allocatable   :: edge_image(:,:)
+    real(kind=dp), allocatable      :: real_image(:,:)
+    
+    call read_pgm('clown.pgm', image)
+    !call write_pgm('clown2.pgm', image)
+    Nx = size(image, 1)
+    Ny = size(image, 2)
+    ! print *, image
+    ! print *, Nx, Ny
+        ! edge_image should be half as wide+1, and equally as deep as image
+    allocate(edge_image(int(Nx/2.0_dp)+1, Ny))
+    allocate(real_image(Nx, Ny))
+    real_image = real(image, kind=dp)
+
+    call dfftw_plan_dft_r2c_2d(plan_forward, Nx, Ny, real_image, edge_image, &
+    & FFTW_ESTIMATE)
+    call dfftw_execute(plan_forward)
+    call dfftw_destroy_plan(plan_forward)
+
+    filter = 45
+    do cx = 1, size(edge_image, 1)
+        do cy = 1, size(edge_image, 2)
+            if ((cx**2 + cy**2) .lt. filter **2)then
+                edge_image(cx, cy) = cmplx(0.0_dp, 0.0_dp, kind=dp)
+            end if
+            if (((size(edge_image, 1) - cx)**2 + cy**2) .lt. filter **2) then
+                edge_image(cx, cy) = cmplx(0.0_dp, 0.0_dp, kind=dp)
+            end if
+        end do 
+    end do
+
+    ! print*, edge_image
+
+    call dfftw_plan_dft_c2r_2d(plan_backward, Nx, Ny, edge_image, real_image, &
+    & FFTW_ESTIMATE)
+    call dfftw_execute(plan_backward)
+    call dfftw_destroy_plan(plan_backward)
+
+    real_image = real_image/(Nx*Ny)
+    call write_pgm('clown_edge.pgm', nint(abs(real_image)))
+end program
---
+++ FFT_Edge/pgm_processing.f90
@@ -0,0 +1,152 @@
+module pgm_processing
+
+    implicit none
+    integer, parameter  :: dp = selected_real_kind(15, 300)
+    private             :: dp
+
+    contains
+
+    !> @brief Finds the next unopened unit
+    !!
+    !! Defaults to starting from 10 but will start from 'unit' if supplied
+    !! Returns -1 if there are no unopened units 10-99
+    !!
+    !! # Parameters:
+    !! integer, optional    :: unit     ! Last known opened unit
+    function newunit(unit)
+
+        integer, optional   :: unit
+        integer             :: newunit
+        logical             :: opened
+
+            ! Start from given unit if supplied, else default to 10
+        if (present(unit)) then
+            newunit = unit
+        else
+            newunit = 10
+        end if
+
+        do
+                ! Iterate through units
+            newunit = newunit + 1
+            inquire(unit = newunit, opened = opened)
+
+                ! Leave loop if unit is unopened
+            if (.not. opened) exit
+                ! Leave loop if all units are occupied
+            if (newunit .ge. 100) exit
+        end do
+
+            ! Return -1 if all options 10-99 are opened
+        if (newunit .ge. 100) newunit = -1
+
+        return
+    end function
+
+
+    !> @brief Looks for given file and, if present, reads data into array
+    !!
+    !! # Parameters:
+    !! character (len=*)    :: filename     ! Name of file to search for
+    !! integer              :: array(:,:)   ! Storage space for pgm data
+    subroutine read_pgm(filename, array)
+
+            ! Input vars
+        character (len=*)       :: filename 
+        integer, allocatable    :: array(:, :)
+
+            ! File utility
+        integer                 :: u, ierr
+        character (len = 50)    :: errmsg
+
+            ! PGM parameters
+        integer :: Nx, Ny
+        integer :: max_greys
+
+            ! Loop counters
+        integer :: i, j, k
+
+            ! Format flags
+        11 format(a2)
+        12 format(i4,i4)
+        13 format(i5)
+        14 format(17(i3.3, 1x))
+
+            ! Ensure array is not allocated
+        if (allocated(array)) deallocate(array)
+
+            ! Open target file in a new unit, and return error code if necessary
+        u = newunit()
+        open(unit=u, file=filename, action='read', iostat=ierr, iomsg=errmsg)
+        if (.not. ierr .eq. 0) write(*,*) trim(errmsg)
+
+        read(u,11)  ! skip pgm 'magic number' line
+
+        Nx = ubound(array, 1) - lbound(array, 1) + 1
+        Ny = ubound(array, 2) - lbound(array, 2) + 1
+        read(u,12) Nx,Ny !width,˜height˜
+        allocate(array(Nx, Ny))
+
+        read(u,13) max_greys !max˜gray˜value˜
+    
+
+        do j=lbound(array, 2), ubound(array, 2)
+            do i=lbound(array, 1), ubound(array, 1) - 17, 17
+                read(u,*) (array(i + k, j), k = 0, 16) !line less than 70 cha
+            end do
+            read(u, 14) (array(k, j), k = i, ubound(array, 1))
+        end do
+
+        close(u)
+    end subroutine
+
+    !> @brief Writes input array data to .pgm format
+    !!
+    !! # Parameters
+    !! character (len=*)    :: filename
+    !! integer              :: array(:,:)
+    subroutine write_pgm(filename, array)
+
+            ! Input vars
+        character (len=*)   :: filename
+        integer             :: array (:,:)
+
+            ! File utility
+        integer             :: u, ierr
+        character(len=50)   :: errmsg
+
+            ! Array data
+        integer             :: Nx, Ny, max_greys
+        integer :: i, j, k
+
+            ! Format flags
+        11 format(a2)
+        12 format(i4,1x,i4)
+        13 format(i5)
+        14 format(17(i3.3, 1x))
+
+            ! Open (or create) target file and return error message if necessary
+        u = newunit()
+        open(unit=u,file=filename, action='write', iostat=ierr, iomsg=errmsg)
+        if (.not. ierr .eq. 0) write(*,*) trim(errmsg)
+
+        write(u,11) 'P2 '    ! pgm 'magic number'
+
+        Nx = ubound(array, 1) - lbound(array, 1) + 1
+        Ny = ubound(array, 2) - lbound(array, 2) + 1
+        write(u,12) Nx,Ny !width,˜height˜
+
+        max_greys = maxval(array)
+        write(u,13) max_greys !max˜gray˜value˜
+    
+
+        do j=lbound(array, 2), ubound(array, 2)
+            do i=lbound(array, 1), ubound(array, 1) - 17, 17
+                write(u, 14) (array(i + k, j), k = 0, 16) !line less than 70 cha
+            end do
+            write(u, 14) (array(k, j), k = i, ubound(array, 1))
+        end do
+
+        close(u)
+    end subroutine
+end module
